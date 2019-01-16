51dcb74ef86d3c5c1e25a916aaddbb3dff622ce8
Richie Bendall <richiebendall@gmail.com>
2019-01-16 22:45:20 +1300

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,1 +1,13 @@
+# Import setuptools module
+import setuptools
 
+# Parse the readme markdown file
+with open("../README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+# Setup module
+setuptools.setup(
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    version_format='{tag}',
+    setup_requires=['setuptools-git-version'])
