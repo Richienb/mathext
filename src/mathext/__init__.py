0bb3f7e6d99d49b8300403713feb3dd613e60e0e
Richie Bendall <richiebendall@gmail.com>
2019-01-24 00:16:08 +1300

    Simplify prime function

--- src/mathext/__init__.py
+++ src/mathext/__init__.py
@@ -525,12 +525,7 @@ def prime(num):
     False
     """
 
-    if num == 1:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+    return num <= 3 or (num + 1) % 6 == 0 or (num - 1) % 6 == 0
 
 
 def getprime(n):
